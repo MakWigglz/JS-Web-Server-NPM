e3196aed8db69924487fefd8cafcb67e7c86f7f8
MakWigglz <wigglz734@gmail.com>
2025-03-08 22:23:35 +0200

    Add files via upload

---
+++ index.579125c3.js
@@ -0,0 +1,18 @@
+const key = "keyName";
+const value = "valueName";
+localStorage.setItem(key, value); // save data
+localStorage.getItem(key); // read data
+localStorage.removeItem(key); // remove data item by key
+localStorage.clear(); // remove all data
+// Examples
+localStorage.setItem("number", 5);
+localStorage.getItem("number");
+localStorage.removeItem("number");
+document.getElementById("app").innerHTML = `
+<h1>Makki the Coder: Using Node.js, NPM, TypeScript, React and JavaScript</h1>
+<h1>...A Working JS Webserver....
+<h1>from www.wigglz.co</h1>
+<h2>by the talented developer who goes by the name Mak the Coder, from Wigglz Pet Food</h1>
+`;
+
+//# sourceMappingURL=index.579125c3.js.map
